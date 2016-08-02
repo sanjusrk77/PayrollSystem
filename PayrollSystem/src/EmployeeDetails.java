27436a83ce4c4eec59093f07dd2887b85ba3baec
sanju.daniel <sanju.daniel@B108-13.soit.local>
2016-08-02 15:56:33 +1200

    Function GetEmployeeDetails added..

--- PayrollSystem/src/EmployeeDetails.java
+++ PayrollSystem/src/EmployeeDetails.java
@@ -5,12 +5,35 @@ public class EmployeeDetails {
 		int Emp_Number;
 	    String Emp_Name;
 	    String Emp_Status;
-	    String Emp_Salary;
+	    int Emp_Salary;
+	    
         public static void main(String[] args){
 			
 			
-			
+        	new EmployeeDetails().GetEmployeeDetails();
 			      
 		}
+        public void GetEmployeeDetails()
+		{
+			
+		
+	        Scanner Empscanner=new Scanner(System.in);
+	        System.out.println("Employee number:");
+	        Emp_Number=Empscanner.nextInt();
+	        System.out.println("Employee name:");
+	        Emp_Name=Empscanner.next();
+	        System.out.println("Status of employee(P/T or F/T)?:");
+	        Emp_Status=Empscanner.next();
+	        System.out.println("salary of employee:");
+	        Emp_Salary=Empscanner.nextInt();
+	        Empscanner.close();
+	        
+	        System.out.println("Employee Number:" + Emp_Number);
+	        System.out.println("Employee Name:" + Emp_Name);
+	        System.out.println("Employe status:" + Emp_Status);
+	        System.out.println("Employee Salary:" + Emp_Salary);
+		}
+	       
+	        
 	    
 }
